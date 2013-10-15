d03ce6ca47c56be33ac35de6ce0f3b53e920362e
Grigory Bordyugov <gregbg@gmail.com>
2013-10-15 13:00:29 +0200

    continuation works with my matrix

--- matrix.py
+++ matrix.py
@@ -1,4 +1,8 @@
-from numpy import dot, zeros_like, zeros
+# the ultimate purpose of this small library of classes is the class for
+# augmented matrices, which are basically nothing but a some ``old'' matrix
+# extended by a column, a row and a number in the lower right corner
+
+from numpy import dot, zeros_like, zeros, array
 from scipy.sparse import csc_matrix
 from scipy.sparse.linalg import factorized
 from scipy.linalg import lu_factor, lu_solve
@@ -22,12 +26,13 @@ class dense_matrix(base_matrix):
   def __init__(self, m):
     base_matrix.__init__(self, m)
 
+    m = array(m)
     self.A     = m
     self.shape = m.shape
 
 
   def factorize(self):
-    if self.factorized:
+    if self.factorized is not None:
       return self.factorized
 
     lu, piv = lu_factor(self.A)
@@ -52,15 +57,16 @@ class sparse_matrix(base_matrix):
   def __init__(self, m):
     base_matrix.__init__(self, m)
 
+    m = csc_matrix(m)
     self.A     = m
     self.shape = m.shape
 
 
   def factorize(self):
-    if self.factorized:
+    if self.factorized is not None:
       return self.factorized
 
-    self.factorized = factorized(csc_matrix(self.A))
+    self.factorized = factorized(self.A)
     return self.factorized
 
 
@@ -75,17 +81,26 @@ class sparse_matrix(base_matrix):
 
 class augmented_matrix(base_matrix):
   def __init__(self, A, B, C, D):
+    """ builds the matrix in the shape of
+
+          A B
+          C D
+
+        where A         is a NxN matrix,
+              D         is a scalar, and
+              B and C   are of corresponding sizes. """
+
     base_matrix.__init__(self, A, B, C, D)
 
-    self.A     = A
-    self.B     = B
-    self.C     = C
-    self.D     = D
+    self.A     =       A
+    self.B     = array(B)
+    self.C     = array(C)
+    self.D     =       D
     self.shape = (A.shape[0]+1, A.shape[1]+1)
 
 
   def factorize(self):
-    if self.factorized:
+    if self.factorized is not None:
       return self.factorized
 
     A, B, C, D = self.A, self.B, self.C, self.D
--- scont.py
+++ scont.py
@@ -1,6 +1,8 @@
-from numpy import array, zeros, sqrt, hstack, vstack, newaxis, dot
+from numpy import array, zeros, sqrt, hstack, vstack, newaxis, dot,\
+                  zeros_like
 from numpy.random import rand
 from numpy.linalg import solve, norm
+from matrix import dense_matrix, sparse_matrix, augmented_matrix
 # from scipy.linalg import lu_factor, lu_solve
 
 ndim = 2
@@ -23,33 +25,39 @@ def f(u, p):
 
 def dfdx(u, p):
   x, y = u[0], u[1]
-  return [[2.0*x, 2.0*y],
-          [1.0,   0.0]]  
+  return dense_matrix([[2.0*x, 2.0*y],
+                       [1.0,   0.0]])
 
 
 
 def dfdp(u, p):
-  return array([0.0, -1.0])
-
+  return [0.0, -1.0]
 
 
 def continuation(f, dfdx, dfdp, x0, p0, nsteps, ds):
   """
   the main function for performing continuation
+
+  res, predictions = continuation(f, dfdx, dfdp, x0, p0, nsteps, ds),
+  where:
+  f    - the nonlinearity
+  dfdx - the Jacobian matrix of the system
+         expected to have a ``factorize'' method, returning a function
+         that represents the inverse operator
   """
 
+  # a couple of helper functions
   def compute_ext_rhs(x, p, x0, p0, xp, pp, ds):
-    """ computes right-hand side of the extenden system """
+    """ computes right-hand side of the extended system """
     return hstack([f(x, p), dot(x-x0, xp) + (p-p0)*pp - ds])
 
 
-  def build_ext_matrix(x, p, tv):
-    """ builds the Jacobian matrix of the extenden system """
-    m = hstack([dfdx(x, p), dfdp(x, p)[:,newaxis]])
-    return vstack([m, tv])
+  def build_ext_matrix(dfdx, x, p, tv):
+    """ builds the Jacobian matrix of the extended system """
+    return augmented_matrix(dfdx, dfdp(x,p), tv[:-1], tv[-1])
 
 
-  def compute_tangent_vector(x, p, old=None):
+  def compute_tangent_vector(dfdx, x, p, old=None):
     """ computes tangent vector along the solution branch """
     if old is not None:
       tv = old
@@ -57,18 +65,24 @@ def continuation(f, dfdx, dfdp, x0, p0, nsteps, ds):
       tv = zeros(ndim+1)
       tv[-1] = 1.0
 
-    m = build_ext_matrix(x, p, tv)
+    m = build_ext_matrix(dfdx, x, p, tv)
 
     b = zeros(ndim+1)
     b[-1] = 1.0
-    tv = solve(m, b)
+    tv = (m.factorize())(b)
+
     return tv/norm(tv)
 
 
+  #
+  # main entry point here
+  # 
   res         = hstack([x0, p0])
   predictions = hstack([x0, p0])
 
-  tv = compute_tangent_vector(x0, p0)
+  jac = dfdx(x0, p0)
+  tv = compute_tangent_vector(jac, x0, p0)
+
   xp = tv[:-1]
   pp = tv[ -1]
 
@@ -86,25 +100,27 @@ def continuation(f, dfdx, dfdp, x0, p0, nsteps, ds):
     nstep = 0
 
     while nrm > tol and nstep < ntst:
-      m = build_ext_matrix(x, p, tv)
+      if nstep > 0:        # otherwise take jac from the computation of
+        jac = dfdx(x, p)   # the tangent vector above
 
+      # perform a solve of the Newton's method
+      m = build_ext_matrix(jac, x, p, tv)
       b = compute_ext_rhs(x, p, x0, p0, xp, pp, ds)
-      du = solve(m, -b)
+      du = (m.factorize())(-b)
 
-      x += du[:-1] # Newton's correction
+      x += du[:-1] # Newton's update of the solution
       p += du[ -1]
       nrm = norm(compute_ext_rhs(x, p, x0, p0, xp, pp, ds))
       print 'nstep', nstep, ',  norm:', nrm
 
       nstep += 1
 
-    if nrm <= tol:
-      print 'converged, stepsize:', ds
-
+    if nrm <= tol: # converged
       x0, p0 = x, p
       res = vstack([res, hstack([x, p])])
 
-      tv = compute_tangent_vector(x, p, tv)
+      tv = compute_tangent_vector(jac, x, p, tv) # jac is already available
+                                                 # and factorized
       xp = tv[:-1]
       pp = tv[ -1]
 
