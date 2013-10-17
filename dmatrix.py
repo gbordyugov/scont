c31d53d59feb20e5bd259497f5cf8867983856da
Grigory Bordyugov <gregbg@gmail.com>
2013-10-17 16:49:14 +0200

    added Fornberg's function weights() to dmatrix

--- dmatrix.py
+++ dmatrix.py
@@ -1,5 +1,47 @@
-from numpy import diag, ones
+from numpy import diag, ones, zeros, array
 from scipy.sparse import lil_matrix
+
+def weights(z, x):# , n, m):
+  """ this is the famous Fornberg's weights routine to compute the coeffs
+      of finite differences approximation.
+
+      Parameters:
+      z    : the x-coordinate where the approximation is needed
+      x    : the array of x-coordinates of the grid points """
+
+  n = len(x)-1
+  m = len(x)-1
+
+  c1 = 1.0
+  c4 = x[0]-z
+
+  c = zeros((n+1, m+1))
+
+  c[0,0] = 1.0
+
+  for i in xrange(1, n+1):
+    mn = min(i, m)
+    c2 = 1.0
+    c5 = c4
+    c4 = x[i]-z
+
+    for j in xrange(i):
+      c3 = x[i]-x[j]
+      c2 = c2*c3
+
+      if j == i-1:
+        for k in xrange(mn,0,-1):
+          c[i,k] = c1*(k*c[i-1,k-1] - c5*c[i-1,k])/c2
+        c[i,0] = -c1*c5*c[i-1,0]/c2
+
+      for k in xrange(mn,0,-1):
+        c[j,k] = (c4*c[j,k] - k*c[j,k-1])/c3
+
+      c[j,0] = c4*c[j,0]/c3
+
+    c1 = c2
+
+  return array(zip(*c))
 
 
 def DMatrixFourthOrder(n, l, perbc):
