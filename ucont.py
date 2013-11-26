835f96353f1089822bfd81b0c56defcd0e448d5e
Grigory Bordyugov <gregbg@gmail.com>
2013-11-26 12:42:40 +0100

    just one file ucont.py

--- ucont.py
+++ ucont.py
@@ -10,52 +10,52 @@ from tip import FindTip as tip
 from f2s import f2s
 
 f = finger.load('fingers/start.finger.11')
+f.dummy = 0.0
+f.nsp = 5
 
 s = f2s(f, 1.0e6, {'nsp': 5, 'dummy': 0.0})
 
-def sector_ucont(s, par1name, par2name, par3name, nsteps, ds):
-  def ue2sector(s, u, p):
-    """ a helper function to map (u, p) --> s"""
-    s.flat[:]        = u[:-2]
-    s.pars[par2name] = u[ -2]
-    s.pars[par3name] = u[ -1]
-    s.pars[par1name] = p
+def ucont(obj, par1name, par2name, par3name, nsteps, ds):
+  def ue2object(obj, u, p):
+    """ a helper function to map (u, p) --> obj"""
+    obj.flat[:]        = u[:-2]
+    obj.pars[par2name] = u[ -2]
+    obj.pars[par3name] = u[ -1]
+    obj.pars[par1name] = p
   
   
-  def dfdpar(s, u, p, dpar, d = 1.0e-3):
+  def dfdpar(obj, u, p, dpar, d = 1.0e-3):
     """ a helper function fo numerically approximating the partial
-        derivative of s with respect to s.pars[dpar], evaluated at (u, p)"""
-    ue2sector(s, u, p)
+        derivative of obj with respect to obj.pars[dpar], evaluated at
+        (u, p)"""
+    ue2object(obj, u, p)
   
-    s.pars[dpar] += d/2.0; rhs2 = s.rhs()
-    s.pars[dpar] -= d    ; rhs1 = s.rhs()
-    s.pars[dpar] += d/2.0
+    obj.pars[dpar] += d/2.0; rhs2 = obj.rhs()
+    obj.pars[dpar] -= d    ; rhs1 = obj.rhs()
+    obj.pars[dpar] += d/2.0
   
     return (rhs2 - rhs1)/d
   
   
   def f(u, p):
-    ue2sector(s, u, p)
-    return npappend(s.rhs(), [0.0, 0.0])
+    ue2object(obj, u, p)
+    return npappend(obj.rhs(), [0.0, 0.0])
   
   
   
   def dfdx(u, p):
-    ue2sector(s, u, p)
-    dfdpar2 = dfdpar(s, u, p, par2name)
-    dfdpar3 = dfdpar(s, u, p, par3name)
+    ue2object(obj, u, p)
+    dfdpar2 = dfdpar(obj, u, p, par2name)
+    dfdpar3 = dfdpar(obj, u, p, par3name)
     
-    # dsdr     = s.drmatrix()    *s.flat
-    # dsdtheta = s.dthetamatrix()*s.flat
-  
-    dsdr     = s.trans1
-    dsdtheta = s.trans2
+    trans1 = obj.trans1
+    trans2 = obj.trans2
   
     if True:
-      t = tip(s.u, s.v)[0]
+      t = tip(obj.u, obj.v)[0]
       i, j = int(t[0]), int(t[1])
   
-      mask = ones(s.shape3, dtype=bool)
+      mask = ones(obj.shape3, dtype=bool)
       nx, ny, nz = mask.shape
   
       k = 20
@@ -64,19 +64,19 @@ def sector_ucont(s, par1name, par2name, par3name, nsteps, ds):
       
       mask[imin:imax, jmin:jmax, :] = False
   
-      mask = mask.reshape(s.shape1)
-      dsdr    [mask] = 0.0
-      dsdtheta[mask] = 0.0
+      mask = mask.reshape(obj.shape1)
+      trans1[mask] = 0.0
+      trans2[mask] = 0.0
   
-    j = sparse_matrix(s.jacobian())
-    j = augmented_matrix(j, dfdpar2, dsdr,     0.0)
-    j = augmented_matrix(j, dfdpar3, dsdtheta, 0.0)
+    j = sparse_matrix(obj.jacobian())
+    j = augmented_matrix(j, dfdpar2, trans1, 0.0)
+    j = augmented_matrix(j, dfdpar3, trans2, 0.0)
   
     return j
   
   
   def dfdp(u, p):
-    return npappend(dfdpar(s, u, p, par1name), [0.0, 0.0])
+    return npappend(dfdpar(obj, u, p, par1name), [0.0, 0.0])
   
   
   fname = 'fort.7'
@@ -98,7 +98,7 @@ def sector_ucont(s, par1name, par2name, par3name, nsteps, ds):
     fort7.write(' '.join(map(str, [p, par2, par3]))+'\n')
     fort7.close()
   
-    flat = u[:-2].reshape(s.shape3)
+    flat = u[:-2].reshape(obj.shape3)
     t = tip(flat[...,0], flat[...,1])[0]
     print 'tip coordinates:', t[0], t[1]
   
@@ -106,14 +106,14 @@ def sector_ucont(s, par1name, par2name, par3name, nsteps, ds):
   
   
   
-  u = zeros(len(s.flat)+2)
-  u[:-2] = s.flat[:]
-  u[ -2] = s.pars[par2name]
-  u[ -1] = s.pars[par3name]
-  p      = s.pars[par1name]
+  u = zeros(len(obj.flat)+2)
+  u[:-2] = obj.flat[:]
+  u[ -2] = obj.pars[par2name]
+  u[ -1] = obj.pars[par3name]
+  p      = obj.pars[par1name]
   
   u, p = continuation(f, dfdx, dfdp, u, p, nsteps, ds, callback) 
-  ue2sector(s, u, p)
-  s.save('sectors/ic.sector')
+  ue2object(obj, u, p)
+  obj.save('objects/ic.object')
 
-  return branch, s
+  return branch, obj
