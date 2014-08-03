9508c2702d81c76b9dca8a5e9c5ddc320db96370
Grigory Bordyugov <gregbg@gmail.com>
2014-08-03 21:31:15 +0200

    e 0.2 0.1 continuation, stepper is still broken

--- eps-cont.py
+++ eps-cont.py
@@ -7,12 +7,14 @@ par2ix = -2
 par3ix = -1
 par1ix =  0
 
-f = finger.load('fingers/ic/e-0.3.finger')
+# f = finger.load('fingers/ic/e-0.3.finger')
+# 
+# def z0(x, p): return p - 0.2
+# branch, f = ucont(f, 'e', 'vy', 'b',  1000, -5.0, [z0])
+# f.save('fingers/ic/e-0.2.finger')
 
-def z0(x, p): return p - 0.2
-branch, f = ucont(f, 'e', 'vy', 'b',  1000, -5.0, [z0])
-f.save('fingers/ic/e-0.2.finger')
-
+f = finger.load('fingers/ic/e-0.2.finger')
+f.reshape(300,200)
 def z1(x, p): return p - 0.1
-branch, f = ucont(f, 'e', 'vy', 'b',  1000, -1.0, [z0])
+branch, f = ucont(f, 'e', 'vy', 'b',  1000, -1.0, [z1])
 f.save('fingers/ic/e-0.1.finger')
